bbd11efdb9ddd2ee31fc4d5c98ecc433fcd0ad97
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-11 22:36:51 -0400

    Commit shell command files for makemore.py

---
+++ train_with_Twain_command.sh
@@ -0,0 +1,2 @@
+python makemore.py -i complete_works_of_mark_twain_gutenberg_with_paragraph_newlines_1a.txt -o twain  --resume --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15
+#python makemore.py -i tiny_sentences.txt -o sentences  --resume --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15

6fe817e364a2daa327ff6516c0ce10b680f0b490
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-12 23:19:11 -0400

    create makemore_sample.py sample_war_and_peace.sh train_war_and_peace.sh train_with_sentences.sh and misc updates

--- train_with_sentences.sh
+++ train_with_sentences.sh
@@ -1,1 +1,1 @@
-python makemore.py -i sentences.txt -o sentences  --resume --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15
+python makemore.py -i sentences/sentences.txt -o sentences --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15

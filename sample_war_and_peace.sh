1f49b68011ee7e9dbd84b501a655cc9379d311e9
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-15 13:40:37 -0400

    Increase layers from 15 to 25; other minor changes

--- sample_war_and_peace.sh
+++ sample_war_and_peace.sh
@@ -1,1 +1,1 @@
-python makemore_sample.py -i war_and_peace/war_and_peace_without_epilogues.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15 --sample-only
+python makemore.py -i war_and_peace/war_and_peace_without_epilogues.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15 --sample-only --resume

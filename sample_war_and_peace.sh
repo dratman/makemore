f83c1f427515c9255d4dc26b6950059a5871dc58
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-15 18:02:07 -0400

    Modify .sh files to specify 40 layers

--- sample_war_and_peace.sh
+++ sample_war_and_peace.sh
@@ -1,1 +1,1 @@
-python makemore.py -i war_and_peace/war_and_peace_without_epilogues.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 15 --sample-only --resume
+python makemore.py -i war_and_peace/war_and_peace_without_epilogues.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 40 --sample-only --resume

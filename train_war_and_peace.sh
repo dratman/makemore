4193159de9923a8f0023886538463d77af7e9080
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-15 21:34:37 -0400

    Miscellaneous incremental enhancements

--- train_war_and_peace.sh
+++ train_war_and_peace.sh
@@ -1,1 +1,1 @@
-python makemore.py -i war_and_peace/war_and_peace_without_epilogues.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 25 --resume
+python makemore.py -i war_and_peace/anna_karenina_war_and_peace_plus_some_Q_and_A.txt -o war_and_peace --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 25 --resume

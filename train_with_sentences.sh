30efd80078d12968b6eb762bd10316cf223789e2
Ralph Dratman <ralph.dratman@gmail.com>
2024-07-17 20:08:07 -0400

    Update train_sentences.sh from '--n-embd 64 --n-embd2 64 --n-head 8 --n-layer 64' to '--n-embd 128 --n-embd2 128 --n-head 8 --n-layer 96' and also commit model.pt as trained

--- train_with_sentences.sh
+++ train_with_sentences.sh
@@ -1,1 +1,1 @@
-python makemore.py -i sentences/sentences.txt -o sentences --device cuda --n-embd 64 --n-embd2 64 --n-head 8 --n-layer 64
+python makemore.py -i sentences/sentences.txt -o sentences --device cuda --n-embd 128 --n-embd2 128 --n-head 8 --n-layer 96
